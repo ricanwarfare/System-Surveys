210d66a6c70b9d06268e9f5a13c497ebfbb422d1
ricanwarfare <33404913+ricanwarfare@users.noreply.github.com>
2026-04-07 16:06:42 -0500

    refactor: replace slow JScript MD5 implementation with batched certutil process hashing

--- win-survey.js
+++ win-survey.js
@@ -7,7 +7,7 @@
 var RESULTS_FILE = "survey_results.txt";
 var ENCODE_OUTPUT = false; // Set to true to Base64 encode the output file
 var EVENT_LOG_LIMIT = 100;
-var ENABLE_PROCESS_HASHING = false; // Set to false by default to prevent JScript hangs on large files
+var ENABLE_PROCESS_HASHING = true; // Use certutil to hash all processes limitlessly
 
 var fso = new ActiveXObject("Scripting.FileSystemObject");
 var shell = new ActiveXObject("WScript.Shell");
@@ -63,114 +63,9 @@ function Section(title) {
     Log(border + "\n");
 }
 
-// --- Pure JScript MD5 Implementation ---
-// Based on Paul Johnston's work (BSD License)
-var MD5 = (function() {
-    function safe_add(x, y) {
-        var lsw = (x & 0xFFFF) + (y & 0xFFFF);
-        var msw = (x >> 16) + (y >> 16) + (lsw >> 16);
-        return (msw << 16) | (lsw & 0xFFFF);
-    }
-    function bit_rol(num, cnt) { return (num << cnt) | (num >>> (32 - cnt)); }
-    function md5_cmn(q, a, b, x, s, t) { return safe_add(bit_rol(safe_add(safe_add(a, q), safe_add(x, t)), s), b); }
-    function md5_ff(a, b, c, d, x, s, t) { return md5_cmn((b & c) | ((~b) & d), a, b, x, s, t); }
-    function md5_gg(a, b, c, d, x, s, t) { return md5_cmn((b & d) | (c & (~d)), a, b, x, s, t); }
-    function md5_hh(a, b, c, d, x, s, t) { return md5_cmn(b ^ c ^ d, a, b, x, s, t); }
-    function md5_ii(a, b, c, d, x, s, t) { return md5_cmn(c ^ (b | (~d)), a, b, x, s, t); }
+// MD5 engine removed. Using certutil mapping instead.
 
-    function binl_md5(x, len) {
-        x[len >> 5] |= 0x80 << (len % 32);
-        x[(((len + 64) >>> 9) << 4) + 14] = len;
-        var a = 1732584193, b = -271733879, c = -1732584194, d = 271733878;
-        for (var i = 0; i < x.length; i += 16) {
-            var olda = a, oldb = b, oldc = c, oldd = d;
-            a = md5_ff(a, b, c, d, x[i + 0], 7, -680876936); d = md5_ff(d, a, b, c, x[i + 1], 12, -389564586);
-            c = md5_ff(c, d, a, b, x[i + 2], 17, 606105819); b = md5_ff(b, c, d, a, x[i + 3], 22, -1044525330);
-            a = md5_ff(a, b, c, d, x[i + 4], 7, -176418897); d = md5_ff(d, a, b, c, x[i + 5], 12, 1200080426);
-            c = md5_ff(c, d, a, b, x[i + 6], 17, -1473231341); b = md5_ff(b, c, d, a, x[i + 7], 22, -45705983);
-            a = md5_ff(a, b, c, d, x[i + 8], 7, 1770035416); d = md5_ff(d, a, b, c, x[i + 9], 12, -1958414417);
-            c = md5_ff(c, d, a, b, x[i + 10], 17, -42063); b = md5_ff(b, c, d, a, x[i + 11], 22, -1990404162);
-            a = md5_ff(a, b, c, d, x[i + 12], 7, 1804603682); d = md5_ff(d, a, b, c, x[i + 13], 12, -40341101);
-            c = md5_ff(c, d, a, b, x[i + 14], 17, -1502002290); b = md5_ff(b, c, d, a, x[i + 15], 22, 1236535329);
-            a = md5_gg(a, b, c, d, x[i + 1], 5, -165796510); d = md5_gg(d, a, b, c, x[i + 6], 9, -1069501632);
-            c = md5_gg(c, d, a, b, x[i + 11], 14, 643717713); b = md5_gg(b, c, d, a, x[i + 0], 20, -373897302);
-            a = md5_gg(a, b, c, d, x[i + 5], 5, -701558691); d = md5_gg(d, a, b, c, x[i + 10], 9, 38016083);
-            c = md5_gg(c, d, a, b, x[i + 15], 14, -660478335); b = md5_gg(b, c, d, a, x[i + 4], 20, -405537848);
-            a = md5_gg(a, b, c, d, x[i + 9], 5, 568446438); d = md5_gg(d, a, b, c, x[i + 14], 9, -1019803690);
-            c = md5_gg(c, d, a, b, x[i + 3], 14, -187363961); b = md5_gg(b, c, d, a, x[i + 8], 20, 1163531501);
-            a = md5_gg(a, b, c, d, x[i + 13], 5, -1444681467); d = md5_gg(d, a, b, c, x[i + 2], 9, -51403784);
-            c = md5_gg(c, d, a, b, x[i + 7], 14, 1735328473); b = md5_gg(b, c, d, a, x[i + 12], 20, -1926607734);
-            a = md5_hh(a, b, c, d, x[i + 5], 4, -378558); d = md5_hh(d, a, b, c, x[i + 8], 11, -2022574463);
-            c = md5_hh(c, d, a, b, x[i + 11], 16, 1839030562); b = md5_hh(b, c, d, a, x[i + 14], 23, -35309556);
-            a = md5_hh(a, b, c, d, x[i + 1], 4, -1530992060); d = md5_hh(d, a, b, c, x[i + 4], 11, 1272893353);
-            c = md5_hh(c, d, a, b, x[i + 7], 16, -155497632); b = md5_hh(b, c, d, a, x[i + 10], 23, -1094730640);
-            a = md5_hh(a, b, c, d, x[i + 13], 4, 681279174); d = md5_hh(d, a, b, c, x[i + 0], 11, -358537222);
-            c = md5_hh(c, d, a, b, x[i + 3], 16, -722521979); b = md5_hh(b, c, d, a, x[i + 6], 23, 76029189);
-            a = md5_hh(a, b, c, d, x[i + 9], 4, -640364487); d = md5_hh(d, a, b, c, x[i + 12], 11, -421815835);
-            c = md5_hh(c, d, a, b, x[i + 15], 16, 530742520); b = md5_hh(b, c, d, a, x[i + 2], 23, -995338651);
-            a = md5_ii(a, b, c, d, x[i + 0], 6, -198630844); d = md5_ii(d, a, b, c, x[i + 7], 10, 1126891415);
-            c = md5_ii(c, d, a, b, x[i + 14], 15, -1416354905); b = md5_ii(b, c, d, a, x[i + 5], 21, -57434055);
-            a = md5_ii(a, b, c, d, x[i + 12], 6, 1700485571); d = md5_ii(d, a, b, c, x[i + 3], 10, -1894946606);
-            c = md5_ii(c, d, a, b, x[i + 10], 15, -1051523); b = md5_ii(b, c, d, a, x[i + 1], 21, -2054922799);
-            a = md5_ii(a, b, c, d, x[i + 8], 6, 1873313359); d = md5_ii(d, a, b, c, x[i + 15], 10, -30611744);
-            c = md5_ii(c, d, a, b, x[i + 6], 15, -1560198380); b = md5_ii(b, c, d, a, x[i + 13], 21, 1309151649);
-            a = md5_ii(a, b, c, d, x[i + 4], 6, -145523070); d = md5_ii(d, a, b, c, x[i + 11], 10, -1120210379);
-            c = md5_ii(c, d, a, b, x[i + 2], 15, 718787280); b = md5_ii(b, c, d, a, x[i + 9], 21, -343485551);
-            a = safe_add(a, olda); b = safe_add(b, oldb); c = safe_add(c, oldc); d = safe_add(d, oldd);
-        }
-        return [a, b, c, d];
-    }
-
-    function rstr2binl(input) {
-        var output = Array(input.length >> 2);
-        for (var i = 0; i < output.length; i++) output[i] = 0;
-        for (var i = 0; i < input.length * 8; i += 8) output[i >> 5] |= (input.charCodeAt(i / 8) & 0xFF) << (i % 32);
-        return output;
-    }
-
-    function binl2hex(binarray) {
-        var hex_tab = "0123456789abcdef";
-        var str = "";
-        for (var i = 0; i < binarray.length * 4; i++) {
-            str += hex_tab.charAt((binarray[i >> 2] >> ((i % 4) * 8 + 4)) & 0xF) +
-                   hex_tab.charAt((binarray[i >> 2] >> ((i % 4) * 8)) & 0xF);
-        }
-        return str;
-    }
-
-    return function(s) { return binl2hex(binl_md5(rstr2binl(s), s.length * 8)); };
-})();
-
-function GetFileHash(path) {
-    try {
-        if (!fso.FileExists(path)) return "N/A (File Missing)";
-        // Pure JScript string manipulation is terribly slow for large binaries. Cap at 1MB to prevent freezing.
-        var fileObj = fso.GetFile(path);
-        if (fileObj.Size > 1 * 1024 * 1024) return "Skipped (>1MB)";
-
-        var stream = new ActiveXObject("ADODB.Stream");
-        stream.Type = 1; // Binary
-        stream.Open();
-        stream.LoadFromFile(path);
-        var binData = stream.Read();
-        stream.Close();
-
-        var dom = new ActiveXObject("Microsoft.XMLDOM");
-        var node = dom.createElement("node");
-        node.dataType = "bin.hex";
-        node.nodeTypedValue = binData;
-        var hex = node.text;
-        
-        // Using an array buffer is exponentially faster than string concatenation in JScript
-        var rawArr = [];
-        for (var i = 0; i < hex.length; i += 2) {
-            rawArr.push(String.fromCharCode(parseInt(hex.substr(i, 2), 16)));
-        }
-        return MD5(rawArr.join(''));
-    } catch (e) {
-        return "Error: " + e.message;
-    }
-}
+// GetFileHash removed; hashing is now optimally batched within SurveyProcesses.
 
 // --- WMI Helpers (Obfuscated Strings) ---
 var _w = "win" + "mgmts" + ":" + "\\\\" + ".\\" + "root" + "\\" + "cim" + "v2";
@@ -278,38 +173,92 @@ function GetFileCompany(path) {
 }
 
 function SurveyProcesses() {
-    Section("Running Processes (MD5 for suspicious paths)");
-    // Increased padding for Name to accommodate [MS] tag
-    Log(Pad("PID", 8) + Pad("Name", 35) + Pad("MD5", 34) + "Path");
-    Log(Pad("---", 8) + Pad("----", 35) + Pad("---", 34) + "----");
+    Section("Running Processes (Batch Certutil Hashing)");
+    var processes = [];
+    var uniquePaths = {};
+    
+    // 1. Collect all running processes
     QueryWMI("SELECT * FROM Win32_Process", function(item) {
         var path = item.ExecutablePath || "N/A";
-        var hash = "N/A";
-        var dispName = item.Name;
-        
-        if (path !== "N/A") {
-            var upperPath = path.toUpperCase();
-            // Flag paths that are common for persistence/malware, avoiding standard Windows directories
-            var isSuspicious = upperPath.indexOf("\\USERS\\") !== -1 ||
-                               upperPath.indexOf("\\PROGRAMDATA\\") !== -1 ||
-                               upperPath.indexOf("\\TEMP\\") !== -1 ||
-                               upperPath.indexOf("\\APPDATA\\") !== -1 ||
-                               upperPath.indexOf("\\PERFLOGS\\") !== -1;
+        if (path !== "N/A" && !uniquePaths[path]) {
+            uniquePaths[path] = true;
+        }
+        processes.push({
+            PID: item.ProcessId,
+            Name: item.Name,
+            Path: path
+        });
+    });
+    
+    // 2. Batch hash unique paths via a single background cmd process
+    var hashMap = {};
+    if (ENABLE_PROCESS_HASHING) {
+        try {
+            var tempDir = shell.ExpandEnvironmentStrings("%TEMP%");
+            var batPath = tempDir + "\\sys_hash_" + Math.floor(Math.random() * 10000) + ".bat";
+            var outPath = tempDir + "\\sys_hash_out_" + Math.floor(Math.random() * 10000) + ".txt";
             
-            if (isSuspicious) {
-                hash = ENABLE_PROCESS_HASHING ? GetFileHash(path) : "Skipped (Disabled)";
+            var batFile = fso.CreateTextFile(batPath, true);
+            batFile.WriteLine("@echo off");
+            for (var p in uniquePaths) {
+                if (fso.FileExists(p)) {
+                    batFile.WriteLine('certutil -hashfile "' + p + '" MD5');
+                }
+            }
+            batFile.Close();
+            
+            // Run entirely hidden (0) and wait to return (true)
+            shell.Run('cmd.exe /c "' + batPath + '" > "' + outPath + '"', 0, true);
+            
+            if (fso.FileExists(outPath)) {
+                var outFile = fso.OpenTextFile(outPath, 1);
+                var output = outFile.AtEndOfStream ? "" : outFile.ReadAll();
+                outFile.Close();
+                
+                var lines = output.split('\n');
+                var currentPath = null;
+                for (var i = 0; i < lines.length; i++) {
+                    var line = lines[i].replace(/\r/g, "");
+                    // Wait for CertUtil "MD5 hash of C:\..." marker
+                    if (line.indexOf("hash of ") !== -1) {
+                        currentPath = line.substring(line.indexOf("hash of ") + 8, line.length - 1);
+                    } else if (currentPath && line.length === 32 && /^[0-9a-fA-F]+$/.test(line.replace(/\s/g, ""))) {
+                        hashMap[currentPath.toLowerCase()] = line.replace(/\s/g, "").toLowerCase();
+                        currentPath = null;
+                    }
+                }
+                fso.DeleteFile(outPath);
+            }
+            if (fso.FileExists(batPath)) fso.DeleteFile(batPath);
+        } catch(e) {
+            Log("Error during batch hashing: " + e.message);
+        }
+    }
+    
+    // 3. Output results
+    Log(Pad("PID", 8) + Pad("Name", 35) + Pad("MD5", 34) + "Path");
+    Log(Pad("---", 8) + Pad("----", 35) + Pad("---", 34) + "----");
+    
+    for (var j = 0; j < processes.length; j++) {
+        var p = processes[j];
+        var dispName = p.Name;
+        var hash = "N/A";
+        
+        if (p.Path !== "N/A") {
+            if (ENABLE_PROCESS_HASHING) {
+                hash = hashMap[p.Path.toLowerCase()] || "N/A";
             } else {
-                hash = "Skipped (Standard)";
+                hash = "Skipped (Disabled)";
             }
             
-            var company = GetFileCompany(path);
+            var company = GetFileCompany(p.Path);
             if (company.indexOf("Microsoft") !== -1) {
                 dispName += " [MS]";
             }
         }
         
-        Log(Pad(item.ProcessId, 8) + Pad(dispName, 35) + Pad(hash, 34) + path);
-    });
+        Log(Pad(p.PID, 8) + Pad(dispName, 35) + Pad(hash, 34) + p.Path);
+    }
 }
 
 function SurveyServices() {
